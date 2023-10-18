9a09acb0a7443f8c79a9974b65380be14abb9619
1015wen <146065580+1015wen@users.noreply.github.com>
2023-10-18 20:57:46 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    
+    if(a%2==0||a%3==0||a%5==0)
+    {
+       printf("NO"); 
+    }
+    else
+    {
+        printf("YES");
+    }
+    
+
+    return 0;
+}
 
