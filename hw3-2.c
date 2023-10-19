83ded47b8c6340e040a5d06bf00da24f88f5b955
1015wen <146065580+1015wen@users.noreply.github.com>
2023-10-19 10:26:25 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+int main()
+{
+    int a,b,c,d,e,f;
+    
+    scanf("%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f);
+    if(b<c&&d<e)
+    {
+        printf("1");
+    }
+    if(b<c&&d>e&&e>=b)
+    {
+        printf("2");
+    }
+    if(b<c&&d>e&&e<b)
+    {
+        printf("3");
+    }
+    if(b>c&&d<e)
+    {
+        printf("1");
+    }
+    if(b>c&&d>e&&e>=b)
+    {
+        printf("2");
+    }
+    if(b>c&&d>e&&e<b)
+    {
+        printf("3");
+    }
+    
+    return 0;
+}
