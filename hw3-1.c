b8c97bc896beec9b254f6d91e809f20c7fbd5c7d
1015wen <146065580+1015wen@users.noreply.github.com>
2023-10-19 10:47:08 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,18 +1,19 @@
-
 #include <stdio.h>
 
 int main()
 {
     int a;
     scanf("%d",&a);
-    
-    if(a%2==0||a%3==0||a%5==0||a%7==0||a%11==0||a%13==0)
+    for(int i=2;i*i<=a;i++)
     {
-       printf("NO"); 
-    }
-    if(a==2||a==3||a==5||a==7||a==11||a==13||a==17||a==19||a==23)
-    {
-        printf("YES");
+        if(a%i==0)
+        {
+            printf("NO");
+        }
+        else
+        {
+            printf("YES");
+        }
     }
     
 
