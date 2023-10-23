6de564ec38ea324f7d944bd04a7c0bcb7a750f41
1015wen <146065580+1015wen@users.noreply.github.com>
2023-10-24 00:16:56 +0800

    Update hw3-3.c

--- hw3-3.c
+++ hw3-3.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int x,y;
+    scanf("%d%d",&x,&y);
+    int SUMx=(x/1000) + ((x%1000)/100) + ((x%100)/10) + (x%10);
+    int SUMy=(y/1000) + ((y%1000)/100) + ((y%100)/10) + (y%10);
+    if(SUMx==SUMy)
+    {
+        if(x<y)
+        {
+            printf("%d",x);
+        }
+
+        else {printf("%d",y);}
+    }
+    else if (SUMx<SUMy)
+    {
+        printf("%d",x);
+    }
+
+    else if(SUMy<SUMy) {printf("%d",y);}
+
+
+
+
+}
